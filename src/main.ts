4a7f80755338cb838fb172244172e08a39854718
Bolaji Oyeneye <bioyeneye@gmail.com>
2020-04-21 16:07:03 +0100

    config port from appmodule

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,20 @@ import { AppModule } from './app.module';
 import { SwaggerDocumentationConfig } from './config/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
-const port = process.env.Port || 3000;
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  //configurations
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
-  SwaggerDocumentationConfig.init(app); 
-  
-  await app.listen(port);
+    //configurations
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
+    SwaggerDocumentationConfig.init(app);
 
-  Logger.log(`Server running on http://localhost:${port}`, 'Bootsrap');
+    await app.listen(AppModule.port, () => {
+        Logger.log(
+            `Server running on http://localhost:${AppModule.port}`,
+            'Bootsrap',
+        );
+    });
 }
+
 bootstrap();
